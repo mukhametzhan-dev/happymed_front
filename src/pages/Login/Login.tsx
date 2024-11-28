cf094f0c009a70e8a9a51506d2a77dbad57767e6
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2024-11-28 13:29:30 +0600

    Update Login.tsx

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -20,7 +20,7 @@ export const Login = () => {
   const onSubmit = async (data) => {
     console.log(data);
     try {
-      const response = await axios.post('http://ant-steady-hugely.ngrok-free.app/login', data);
+      const response = await axios.post('https://ant-steady-hugely.ngrok-free.app/login', data);
       console.log('Login response:', response.data);
 
       if (response.data.status === 'success') {
