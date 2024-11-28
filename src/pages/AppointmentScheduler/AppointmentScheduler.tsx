6649d1899d87d51a4aa426219c0f47ce37121db0
Mukhamedzhan <128323894+mukhametzhan-dev@users.noreply.github.com>
2024-11-28 13:05:07 +0600

    Update AppointmentScheduler.tsx

--- src/pages/AppointmentScheduler/AppointmentScheduler.tsx
+++ src/pages/AppointmentScheduler/AppointmentScheduler.tsx
@@ -58,7 +58,7 @@ const AppointmentScheduler = ({ doctorEmail }) => {
     try {
       // Submit the appointmentData as JSON
       const response = await axios.post(
-        'http://127.0.0.1:5000/make_appointment',
+        'http://ant-steady-hugely.ngrok-free.app/make_appointment',
         appointmentData,
         {
           headers: {
